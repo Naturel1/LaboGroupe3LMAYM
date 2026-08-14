ec2b753665d0f10be3632593375b6375072b7f45
Mara Lurkin <mara.lurkin@hotmail.fr>
2026-08-14 15:48:30 +0200

    add: services/site_service.py

--- app/services/site_service.py
+++ app/services/site_service.py
@@ -1,8 +1,13 @@
-from app import app, db
+from app import db
 from app.models.site import Site
+from app.framework.decorators.injectable import injectable
+from app.forms.site.site_form import SiteForm
+from app.services.base_service import BaseService
+from app.mappers.site_mapper import SiteMapper
 
 
-class SiteService:
+@injectable
+class SiteService(BaseService):
     def find_all(self):
         """Find all sites."""
         return Site.query.filter_by(active=True).all()
@@ -11,24 +16,24 @@ class SiteService:
         """Find one site by id."""
         return Site.query.filter_by(site_id=site_id, active=True).first()
 
-    def insert(self, form):
+    def find_one_by(self, **kwargs):
+        """Find one site by any field."""
+        return Site.query.filter_by(**kwargs).first()
+    
+    def insert(self, form: SiteForm):
         """Create a new site."""
         site = Site()
-        site.site_name = form.name.data
-        site.site_address = form.address.data
-        site.site_city = form.city.data
+        site = SiteMapper.form_to_entity(form, site)
         db.session.add(site)
         db.session.commit()
         return site
 
-    def update(self, site_id, form):
+    def update(self, site_id, form: SiteForm):
         """Update a site attributes."""
         site = self.find_one(site_id)
         if site is None:
             return None
-        site.site_name = form.name.data
-        site.site_address = form.address.data
-        site.site_city = form.city.data
+        site = SiteMapper.form_to_entity(form, site)
         db.session.commit()
         return site
 
