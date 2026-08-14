cc4d7580ba16a3a9c79fdc7f75ae3deca8ad3117
Mara Lurkin <mara.lurkin@hotmail.fr>
2026-08-14 14:30:28 +0200

    add: forms/equipment/equipment_form.py

---
+++ app/forms/equipment/equipment_form.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, DateField, IntegerField
+from wtforms.validators import DataRequired, Length
+
+class EquipmentForm(FlaskForm):
+    name = StringField("Equipment name", validators=[
+        DataRequired(),
+        Length(min=2, max=100)
+    ])
+    type = StringField("Type", validators=[
+        DataRequired(),
+        Length(min=2, max=80)
+    ])
+    serial = StringField("Serial Number", validators=[
+        DataRequired(),
+        Length(min=2, max=100)
+    ])
+    purchase_date = DateField("Purchase Date", validators=[
+        DataRequired()
+    ])
+    site_id = IntegerField("Site", validators=[
+        DataRequired()
+    ])
+    user_id = IntegerField("Assigned User", validators=[])
