424ae0df4d5d36a4d40162817c77b5b5858d8da3
Mara Lurkin <mara.lurkin@hotmail.fr>
2026-08-14 16:04:35 +0200

    add: SiteDTO.py + fix: site_service.py with DTO

---
+++ app/mappers/site_mapper.py
@@ -0,0 +1,18 @@
+from app.models.site import Site
+from app.mappers.abstract_mapper import AbstractMapper
+from app.dtos.site_dto import SiteDTO
+from app.forms.site.site_form import SiteForm
+
+
+class SiteMapper(AbstractMapper):
+    
+    @staticmethod
+    def entity_to_dto(entity: Site) -> SiteDTO:
+        return SiteDTO.build_from_entity(entity)
+
+    @staticmethod
+    def form_to_entity(form: SiteForm, entity: Site):
+        entity.site_name = form.name.data
+        entity.site_address = form.address.data
+        entity.site_city = form.city.data
+        return entity
--- app/services/site_service.py
+++ app/services/site_service.py
@@ -10,23 +10,25 @@ from app.mappers.site_mapper import SiteMapper
 class SiteService(BaseService):
     def find_all(self):
         """Find all sites."""
-        return Site.query.filter_by(active=True).all()
+        return [SiteMapper.entity_to_dto(site)
+                for site in Site.query.filter_by(active=True).order_by(Site.site_id).all()]
 
     def find_one(self, site_id):
         """Find one site by id."""
-        return Site.query.filter_by(site_id=site_id, active=True).first()
+        site = self.find_one_entity(site_id)
+        return SiteMapper.entity_to_dto(site) if site else None
 
     def find_one_by(self, **kwargs):
         """Find one site by any field."""
-        return Site.query.filter_by(**kwargs).first()
-    
+        return Site.query.filter_by(active=True, **kwargs).first()
+
     def insert(self, form: SiteForm):
         """Create a new site."""
         site = Site()
         site = SiteMapper.form_to_entity(form, site)
         db.session.add(site)
         db.session.commit()
-        return site
+        return SiteMapper.entity_to_dto(site)
 
     def update(self, site_id, form: SiteForm):
         """Update a site attributes."""
@@ -35,7 +37,7 @@ class SiteService(BaseService):
             return None
         site = SiteMapper.form_to_entity(form, site)
         db.session.commit()
-        return site
+        return SiteMapper.entity_to_dto(site)
 
     def delete(self, site_id):
         """Soft delete a site"""
